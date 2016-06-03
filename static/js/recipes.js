e9f18c9e2a4764e6a566653f5cff003b4408570b
Katie Tarng <katietarng@gmail.com>
2016-06-02 20:43:11 -0700

    Added style to homepage and modal window

--- static/js/recipes.js
+++ static/js/recipes.js
@@ -12,12 +12,12 @@ function showDetails(e) {
     $.get("/recipe-details.json",
         details,
         function (result) {
-            var recipeHtml = 'Cook Time: ' + result.info.cooktime +
-                             ' minutes' + '<br>Matched Ingredients: ' +
+            var recipeHtml = '<b>Cook Time:</b> ' + result.info.cooktime +
+                             ' minutes' + '<br><b>Matched Ingredients:</b> ' +
                              '<br>';
 
             $("#modal-" + result.id).find(".modal-title").html(result.title);
-            $("#modal-" + result.id).find(".modal-body .image").attr("src", result.image);
+            $("#modal-" + result.id).find(".modal-body .modal-image").css("background-image", "url('" + result.image + "')");
             $("#modal-" + result.id).find(".modal-body .description").html(recipeHtml);
             $("#modal-" + result.id).find(".cook").attr("data-source", result.info.source);
             $("#modal-" + result.id).find(".cook").attr("data-ing", result.info.ingredients);
@@ -77,10 +77,10 @@ $(function () {
     $('.cook')
         .on('click', addRecipe);
 
-    $(".recipe").slice(0, 5).css("display", "inline-block");
+    $(".recipe").slice(0, 6).css("display", "inline-block");
     $("#load").click(function(e){
         e.preventDefault();
-        $(".recipe:hidden").slice(0, 5).css("display", "inline-block");
+        $(".recipe:hidden").slice(0, 6).css("display", "inline-block");
         if($(".recipe:hidden").length === 0){
             $("#load").hide();
         }
